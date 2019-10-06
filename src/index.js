9e37ec6deb054dbce85f52cf6aee0ac370e2e34d
hamid842 <h_mohammadi842@yahoo.com>
2019-10-07 00:16:02 +0330

    First commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
+// React => is a javascript library for building user interfaces
+// Component => is a peice of the UI
+// React application is a tree of components
+
+
+import React from 'react';           // import a couple of Objects from some modules
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const element = <h1>Hello there</h1>;  // JSX expression
+ReactDOM.render(element , document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// when don't need to run npm on every change of code . 
+// when save the code and refresh the browser can see the changes .
+// It's called "Hot-Module-Reloading"
+
+
+
+
